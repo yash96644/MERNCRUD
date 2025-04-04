e55a416d3ec76ac70e9b5984046c5604a50a3ba8
yash96644 <yg095544@gmail.com>
2025-04-04 20:50:35 +0530

    added delete functionality

--- client/curd/src/components/Read.jsx
+++ client/curd/src/components/Read.jsx
@@ -17,6 +17,24 @@ async function getData() {
   }
 }
 
+const handleDelete = async (id) =>{
+      const response = await fetch(`http://localhost:5000/${id}`, {
+        method: "DELETE",
+      });
+      const result = await response.json();
+      if(!response.ok) {
+        setError("Failed to fetch data:", result.error);
+      }
+      if(response.ok) {
+        setError("data deleted successfully");
+        setTimeout(() => {
+          setError("");
+          getData();
+        }
+        , 2000);
+      }
+}
+
 useEffect(() => {
   getData();
 }
@@ -40,7 +58,7 @@ console.log(data);
                       <h5 className="card-title">{ele.name}</h5>
                       <h6 className="card-subtitle mb-2 text-body-secondary">{ele.email}</h6>
                       <p className="text-muted">{ele.age}</p>
-                      <a href="#" className="card-link">
+                      <a href="#" className="card-link" onClick={() => {handleDelete(ele._id)}}>
                         delete
                       </a>
                       <a href="#" className="card-link">
